a5e0f935538811a2b0952177f76839fbb6e2a068
bitab <bisratbetre5@gmail.com>
2019-05-09 17:18:00 +0300

    Revert "changed the printed text to ertyuiop"
    This reverts commit 0091cc5e09d76a7346c781d0f7d5d886a1738ddc.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ertyuiop");
+        System.out.println("Hello World!");
          System.out.println("Check Github");
-
     }
 }
