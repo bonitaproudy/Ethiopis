e474256cefe82409cafeb8aab8ae863c4f4fd840
Abraham Terfie <abrahamterfie1236@gmail.com>
2019-05-09 17:32:19 +0300

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-         System.out.println("Check Github");
+        System.out.print("am here now");
     }
 }
