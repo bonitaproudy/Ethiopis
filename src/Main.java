1bf4abbe679a865589c2813d975d782e315a14d5
rav3n11 <rav3n11@users.noreply.github.com>
2019-05-11 11:59:12 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("am here now");
     }
+
+    static class GUI {
+        //The Gui class... obviously
+        //I'm using javaFx... y'all okay with that?
+    }
 }
+
