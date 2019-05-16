e53c59cedf4fd9a950a6e4c9fe7ceb49c52c9730
rav3n11 <rav3n11@users.noreply.github.com>
2019-05-16 10:02:58 +0300

    first few pages

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+
+import javax.swing.text.Element;
+import javax.swing.text.html.ImageView;
+
+public class Controller {
+
+    public Button externalsettings;
+
+    public Button getExternalsettings() {
+        return externalsettings;
+    }
+}
